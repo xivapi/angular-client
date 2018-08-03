0ea4b5cacfa90c342c500ff718b72a638cfab8ec
supamiu <contact@flavien-normand.fr>
2018-08-03 14:56:52 +0200

    chore: exports fixes

--- index.ts
+++ index.ts
@@ -1,15 +1,4 @@
 // Public classes.
 export { XivapiClientModule, XIVAPI_KEY } from './src/xivapi-client.module';
 export { XivapiService } from './src/xivapi.service';
-export {
-    Pagination,
-    SearchAlgo,
-    SearchIndex,
-    XivapiCharacterOptions,
-    XivapiEndpoint,
-    XivapiFreeCompanyOptions,
-    XivapiList,
-    XivapiOptions,
-    XivapiRequestOptions,
-    XivapiSearchOptions
-} from './src/model/index';
+export * from './src/model/index';
