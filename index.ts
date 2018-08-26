3cdbe624cdabc949bd361e276e10024343e173c0
Supamiu <contact@flavien-normand.fr>
2018-08-26 16:08:13 +0200

    fix: import failing in app.module.ts files

--- index.ts
+++ index.ts
@@ -1,4 +1,1 @@
-// Public classes.
-export { XivapiClientModule, XIVAPI_KEY } from './src/xivapi-client.module';
-export { XivapiService } from './src/xivapi.service';
-export * from './src/model/index';
+export * from './public_api';
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { XivapiClientModule, XIVAPI_KEY } from './xivapi-client.module';
+export { XivapiService } from './xivapi.service';
