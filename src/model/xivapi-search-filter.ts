15c0a69e522481f756e3ead3d4d246434de7e320
Supamiu <contact@flavien-normand.fr>
2020-12-30 12:22:08 +0100

    feat(search): support for field exclusion

--- src/model/xivapi-search-filter.ts
+++ src/model/xivapi-search-filter.ts
@@ -1,5 +1,5 @@
 export interface XivapiSearchFilter {
     column: string;
-    operator: '=' | '>' | '<' | '<=' | '>=' | '|=';
-    value: string | number | string[] | number[];
+    operator: '=' | '>' | '<' | '<=' | '>=' | '|=' | '!';
+    value?: string | number | string[] | number[];
 }
